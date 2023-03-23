5e3d994c82cc6e5608839ce4cbccdaaba0211945
Rikuto <orikutobasuke@gmail.com>
2023-03-23 16:48:24 +0900

    team-A add

--- public/js/index01.js
+++ public/js/index01.js
@@ -1,8 +1,89 @@
 const btn = document.querySelectorAll('.btn');
-console.log(btn);
+const text = document.querySelectorAll('.chatText');
+const box = document.querySelector('.chatbox');
+const message = document.querySelectorAll('.message')
+console.log(btn, text, box);
 
-btn.forEach(e => {
-    e.addEventListener('click', () => {
-        e.style.transform = 'scale(1.2,1.5)'
+const chat = 'chat.json'
+
+
+fetch(chat)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+        // for (let i = 0; i < text.length; i++) {
+        //     text[i].innerHTML = json[i].text
+        // }
+        json.forEach(e => {
+            let chat = `
+                    <div class="message">
+                        <p class="chatText">${e.text}</p>
+                    </div>
+                `
+            box.innerHTML += chat
+        });
+        for (let i = 0; i < btn.length; i++) {
+            let flg = false
+            btn[0].addEventListener('click', (e) => {
+                btn[i].classList.remove('btnstyle');
+                btn[0].classList.add('btnstyle');
+            })
+            btn[1].addEventListener('click', (e) => {
+                btn[i].classList.remove('btnstyle');
+                btn[1].classList.add('btnstyle');
+            })
+            btn[2].addEventListener('click', (e) => {
+                btn[i].classList.remove('btnstyle');
+                btn[2].classList.add('btnstyle');
+            })
+        }
+        btn[0].addEventListener('click', () => {
+            const message = document.querySelectorAll('.message');
+            message.forEach(e => {
+                e.remove();
+            })
+            json.forEach(e => {
+                if (e.flg === 1) {
+                    let chat = `
+                        <div class="message">
+                            <p class="chatText">${e.text}</p>
+                        </div>
+                    `
+                    box.innerHTML += chat
+                }
+            })
+        })
+        btn[1].addEventListener('click', () => {
+            const message = document.querySelectorAll('.message');
+            message.forEach(e => {
+                e.remove();
+            })
+            json.forEach(e => {
+                if (e.flg === 2) {
+                    let chat = `
+                        <div class="message">
+                            <p class="chatText">${e.text}</p>
+                        </div>
+                    `
+                    box.innerHTML += chat
+                }
+            })
+        })
+        btn[2].addEventListener('click', () => {
+            const message = document.querySelectorAll('.message');
+            message.forEach(e => {
+                e.remove();
+            })
+            json.forEach(e => {
+                if (e.flg === 3) {
+                    let chat = `
+                        <div class="message">
+                            <p class="chatText">${e.text}</p>
+                        </div>
+                    `
+                    box.innerHTML += chat
+                }
+            })
+        })
     })
-});
+
